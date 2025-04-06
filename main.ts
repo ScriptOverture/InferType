4fdef36cfec0457f4eaf44875a35822a1b33c50d
scriptOverture <laoju34102uy@163.com>
2025-04-06 19:28:37 +0800

    feat: 匹配參數類型

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Project, SyntaxKind } from "ts-morph";
+import { ParameterDeclaration, Project, SyntaxKind } from "ts-morph";
 
 export function inferFunctionType(
     sourceStr: string,
@@ -8,13 +8,32 @@ export function inferFunctionType(
     const sourceFile = project.createSourceFile("temp.ts", sourceStr);
     let iFunction = sourceFile.getFunction(targetFuncName)!;
     if (iFunction) {
-        return iFunction.getParameters().length;
+        return getAllParametersType(iFunction.getParameters());
     } else {
         // 获取变量声明（即函数表达式所在的位置）
         const variableDeclaration = sourceFile.getVariableDeclaration(targetFuncName);
         const initializer = variableDeclaration?.getInitializer();
-
         const funParams = variableDeclaration?.getInitializerIfKind(initializer?.getKind()! as SyntaxKind.FunctionExpression);
-        return funParams?.getParameters()?.length;
+        return getAllParametersType(funParams?.getParameters()!);
     }
+}
+
+interface ParamsResult {
+    params: Partial<Record<string, string>>;
+    length: number;
+  }
+
+function getAllParametersType(parasms: ParameterDeclaration[]) {
+    return parasms.reduce<ParamsResult>((result, item) => {
+        return {
+            ...result,
+            params: {
+                ...result.params,
+                [item.getName()]: item.getType().getText()
+            }
+        }
+    }, {
+        params: {},
+        length: parasms.length
+    });
 }
--- tests/funParamsLen.test.ts
+++ tests/funParamsLen.test.ts
@@ -1,7 +1,5 @@
 import { expect, test, describe } from "bun:test";
-import {inferFunctionType} from '../main'
-import {Project} from "ts-morph";
-
+import { inferFunctionType } from '../main'
 
 describe("检查函数参数是否正常", () => {
     const funName = 'test';
@@ -9,43 +7,43 @@ describe("检查函数参数是否正常", () => {
 
     test("函数申明参数", () => {
         const origin = `function ${funName}(${params.join(',')}) {}`;
-        const paramsLen = inferFunctionType(origin, funName);
+        const paramsLen = inferFunctionType(origin, funName).length;
         expect(paramsLen).toBe(params.length);
     })
 
     test("箭头函数参数[const]", () => {
         const origin = `const ${funName} = (${params.join(',')}) => {}`;
-        const paramsLen = inferFunctionType(origin, funName);
+        const paramsLen = inferFunctionType(origin, funName).length;
         expect(paramsLen).toBe(params.length);
     })
 
     test("箭头函数参数[var]", () => {
         const origin = `var ${funName} = (${params.join(',')}) => {}`;
-        const paramsLen = inferFunctionType(origin, funName);
+        const paramsLen = inferFunctionType(origin, funName).length;
         expect(paramsLen).toBe(params.length);
     })
 
     test("箭头函数参数[let]", () => {
         const origin = `let ${funName} = (${params.join(',')}) => {}`;
-        const paramsLen = inferFunctionType(origin, funName);
+        const paramsLen = inferFunctionType(origin, funName).length;
         expect(paramsLen).toBe(params.length);
     })
 
     test("函数表达式参数[const]", () => {
         const origin = `const ${funName} = function (${params.join(',')}) {}`;
-        const paramsLen = inferFunctionType(origin, funName);
+        const paramsLen = inferFunctionType(origin, funName).length;
         expect(paramsLen).toBe(params.length);
     })
 
     test("函数表达式参数[var]", () => {
         const origin = `var ${funName} = function (${params.join(',')}) {}`;
-        const paramsLen = inferFunctionType(origin, funName);
+        const paramsLen = inferFunctionType(origin, funName).length;
         expect(paramsLen).toBe(params.length);
     })
 
     test("函数表达式参数[let]", () => {
         const origin = `let ${funName} = function (${params.join(',')}) {}`;
-        const paramsLen = inferFunctionType(origin, funName);
+        const paramsLen = inferFunctionType(origin, funName).length;
         expect(paramsLen).toBe(params.length);
     })
 });
