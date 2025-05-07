7ace1edb83ab8415bac55cfc15016ba7c9f76a13
scriptOverture <laoju34102uy@163.com>
2025-05-07 22:52:02 +0800

    fix: 补充eslint配置

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default tseslint.config(
+    // 基础配置
+    {
+      files: ["lib/**/*.ts", "main.ts"],
+      plugins: {
+        "@typescript-eslint": tseslint.plugin
+      },
+      languageOptions: {
+        globals: {
+          ...globals.browser,
+          ...globals.node
+        }
+      }
+    },
+
+    // 继承推荐配置
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    // 自定义规则覆盖
+    {
+      files: ["lib/**/*.ts", "main.ts"],
+      rules: {
+        "@typescript-eslint/no-non-null-asserted-optional-chain": "off"
+      }
+    }
+);
